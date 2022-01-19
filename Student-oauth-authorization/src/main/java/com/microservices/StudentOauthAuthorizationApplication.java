7d6631bfd0e15f75b4e7df88ebe56f71383c33af
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-01-19 14:11:06 +0530

    Adding Oauth Authorization project

---
+++ Student-oauth-authorization/src/main/java/com/microservices/StudentOauthAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentOauthAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentOauthAuthorizationApplication.class, args);
+	}
+
+}
