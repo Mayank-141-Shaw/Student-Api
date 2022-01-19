721b2bff904749cb72b84166fa03840a5217d5e9
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-01-19 14:35:15 +0530

    Adding resource

---
+++ Student-oauth-resource/src/main/java/com/microservices/StudentOauthResourceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentOauthResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentOauthResourceApplication.class, args);
+	}
+
+}
