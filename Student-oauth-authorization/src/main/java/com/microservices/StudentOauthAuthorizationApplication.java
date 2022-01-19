38109dac7f72c0a5a0998ec5ec97194a82de0d2b
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-01-19 14:33:45 +0530

    Adding oauth

--- Student-oauth-authorization/src/main/java/com/microservices/StudentOauthAuthorizationApplication.java
+++ Student-oauth-authorization/src/main/java/com/microservices/StudentOauthAuthorizationApplication.java
@@ -1,13 +1,24 @@
 package com.microservices;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableResourceServer
 @SpringBootApplication
 public class StudentOauthAuthorizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentOauthAuthorizationApplication.class, args);
 	}
-
+	
+	@RequestMapping("/validateUser")
+	public Principal user(Principal user) {
+		return user;
+	}
 }
