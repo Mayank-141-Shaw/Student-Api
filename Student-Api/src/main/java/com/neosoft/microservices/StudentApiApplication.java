bc6933a730d15764bed73ec8d0325ac1dc4ad262
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-01-19 13:09:51 +0530

    Added default roles through command line runner in main app.

--- Student-Api/src/main/java/com/neosoft/microservices/StudentApiApplication.java
+++ Student-Api/src/main/java/com/neosoft/microservices/StudentApiApplication.java
@@ -1,13 +1,33 @@
 package com.neosoft.microservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class StudentApiApplication {
+import com.neosoft.microservices.model.Role;
+import com.neosoft.microservices.repo.RoleDao;
 
+@SpringBootApplication
+public class StudentApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	private RoleDao roleDao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Role adminRole = new Role();
+		Role userRole = new Role();
+		adminRole.setName("ADMIN");
+		userRole.setName("USER");
+		
+		roleDao.save(adminRole);
+		roleDao.save(userRole);
 	}
 
 }
